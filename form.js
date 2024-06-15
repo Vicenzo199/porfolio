51ca2460e7cda022d6cd7e6a6acb2452422e78bf
Vicenzo199 <136687874+Vicenzo199@users.noreply.github.com>
2024-06-15 08:52:58 +0200

    Add files via upload

---
+++ form.js
@@ -0,0 +1,26 @@
+
+
+    // storing data
+
+    // var firstNme = localStorage.setItem("fname","SEMANA");
+    // var lastName = localStorage.setItem("lname", "VINCENT");
+    // var gender = localStorage.setItem('gender', 'Male');
+    // var gender = localStorage.setItem('gender', 'Female');
+    // var Birth = localStorage.setItem("Birthday", "02/02/2020");
+    // var email = localStorage.setItem("Gmail", "Vicenzomarc10@gmail.com");
+
+    // retrieving data
+
+//     var first =  localStorage.getItem("fname");
+//     var second = localStorage.getItem("lname");
+//      var gender = localStorage.getItem('gender');
+//     var gender = localStorage.getItem('gender');
+//     var Birth = localStorage.getItem("Birthday");
+//     var email = localStorage.getItem("Gmail");
+// 
+// document.addEventListener('DOMContentLoaded', function() {
+//     var modal = document.querySelectorAll('.auth');
+//     M.auth.init(auth);
+// })
+
+
